47e1bf70a4d0081d046e5967668f9bea54a6786e
Leo Izen <leo.izen@gmail.com>
2021-08-18 18:43:20 -0400

    ranger: use dbs module

--- ranger.py
+++ ranger.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
 import discord
+
 from deepbluesky import DeepBlueSky
 
 # Launch a default Deep Blue Sky bot
 
-client = DeepBlueSky()
+client = DeepBlueSky(bot_name='ranger')
 
 client.default_properties['command_prefix'] = '-'
 client.extra_wikis.append('https://azurlane.koumakan.jp/w/index.php')
-
-@client.event
-async def on_ready():
-    client.logger.info(f'Logged in as {client.user}')
-    game = discord.Game('-help')
-    await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(message):
